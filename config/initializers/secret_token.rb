f5bd41f6c5e460a5b4d90f0cd2e24c6b152367bf
Steve Schwartz <steve@alfajango.com>
2011-05-08 15:08:32 -0400

    First commit. Rails app + jquery-ujs + Devise + Rspec.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Jshub::Application.config.secret_token = 'ea17a715fa5721a5a9445d4fe5e560bcee8e83ebf0f99188dbca958fe9c4ed54ae14a4fea066aebe59151a574cb5090e20820aab8546ecc4b6aa7e563f18f561'
